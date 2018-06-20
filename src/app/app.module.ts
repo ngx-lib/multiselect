8c6296171e019893874b7121ce659118c45c952b
Pankaj Parkar <parkarpankaj3@gmail.com>
2018-06-20 23:56:47 +0530

    Added inital single select skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { IstevenMultiselectModule } from 'isteven-multiselect';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    IstevenMultiselectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
