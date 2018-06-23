524d265cf337096d54ddb39c630df57495f44fbe
Pankaj Parkar <parkarpankaj3@gmail.com>
2018-06-23 14:24:35 +0530

    Added file watch for library so that when changes are made in the library that will be reflected on the Page.

--- projects/isteven-multiselect/src/lib/isteven-multiselect.module.ts
+++ projects/isteven-multiselect/src/lib/isteven-multiselect.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { IstevenMultiselectComponent } from './isteven-multiselect.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   imports: [
+    CommonModule
   ],
   declarations: [IstevenMultiselectComponent],
   exports: [IstevenMultiselectComponent]
