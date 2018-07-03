710230ea466dd8e9d5f0dabba2d458574f77e209
Pankaj Parkar <parkarpankaj3@gmail.com>
2018-07-03 08:47:06 +0530

    moved controlvalueaccessor changes to base component

--- projects/isteven-multiselect/src/lib/isteven-multiselect-base.component.ts
+++ projects/isteven-multiselect/src/lib/isteven-multiselect-base.component.ts
@@ -2,7 +2,7 @@ import { ElementRef, Injector } from '@angular/core';
 import { IstevenMultiselectService } from './isteven-multiselect.service';
 import { ControlValueAccessor } from '@angular/forms';
 
-export class IstevenMultiselectBaseComponent {
+export class IstevenMultiselectBaseComponent implements ControlValueAccessor {
   private _istevenMultiselectService: IstevenMultiselectService;
   private _elementRef: ElementRef;
 
@@ -15,4 +15,16 @@ export class IstevenMultiselectBaseComponent {
   onChange = (_: any) => {};
   onTouched = () => {};
 
+  writeValue(value) { 
+    console.log("writeValue", value)
+  }
+  registerOnChange(fn: (value: any) => any): void { 
+    console.log("registerOnChange", fn)
+    this.onChange = fn; 
+  }
+
+  registerOnTouched(fn: () => any): void { 
+    console.log("registerOnTouched", fn)
+    this.onTouched = fn; 
+  }
 }
