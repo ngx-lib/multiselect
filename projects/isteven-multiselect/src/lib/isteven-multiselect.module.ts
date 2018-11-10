1cc166ea562177843b2f3b23f09c25b03f92404f
Pankaj Parkar <parkarpankaj3@gmail.com>
2018-11-10 19:30:46 +0530

    Added helper-elements component.

--- projects/isteven-multiselect/src/lib/isteven-multiselect.module.ts
+++ projects/isteven-multiselect/src/lib/isteven-multiselect.module.ts
@@ -3,6 +3,7 @@ import { IstevenMultiselectComponent } from './isteven-multiselect.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DisplaySelectedValuePipe } from './display-selected-value.pipe';
+import { HelperElementsComponent } from './helper-elements/helper-elements.component';
 
 @NgModule({
   imports: [
@@ -10,7 +11,11 @@ import { DisplaySelectedValuePipe } from './display-selected-value.pipe';
     FormsModule,
     ReactiveFormsModule
   ],
-  declarations: [IstevenMultiselectComponent, DisplaySelectedValuePipe],
+  declarations: [
+    IstevenMultiselectComponent, 
+    DisplaySelectedValuePipe, 
+    HelperElementsComponent
+  ],
   exports: [IstevenMultiselectComponent]
 })
 export class IstevenMultiselectModule { }
---
+++ src/app/projects/isteven-multiselect/src/lib/helper-elements/helper-elements.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'im-helper-elements',
+  templateUrl: './helper-elements.component.html',
+  styleUrls: ['./helper-elements.component.css']
+})
+export class HelperElementsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
