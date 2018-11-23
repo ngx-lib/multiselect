3a0685ae834802303840e7d213693fd15750a0ca
Pankaj Parkar <parkarpankaj3@gmail.com>
2018-11-23 23:53:03 +0530

    Added `Multiple Select (With Observable)` on testing page.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,9 @@ export class AppComponent {
   groupingSupport = new FormControl([
     {id: 1, name: 'Test 1'},
   ]);
+  withObservable = new FormControl([
+    {id: 1, name: 'Test 1'},
+  ]);
   // options = [
   //   'a', 'b', 'c', 'd', 'e', 'f'
   // ]
@@ -32,6 +35,14 @@ export class AppComponent {
   ]
 
   multipleSelectOptions = [
+    {id: 1, name: 'Test 1'},
+    {id: 2, name: 'Test 2'},
+    {id: 3, name: 'Test 3'},
+    {id: 4, name: 'Test 4'},
+    {id: 5, name: 'Test 5'}
+  ]
+
+  observableOptions = [
     {id: 1, name: 'Test 1'},
     {id: 2, name: 'Test 2'},
     {id: 3, name: 'Test 3'},
