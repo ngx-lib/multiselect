81025290cd3276ce786b73acc4daa6ddd5feda7a
Pankaj Parkar <parkarpankaj3@gmail.com>
2018-11-25 00:05:33 +0530

    Added support for queueing operation and resume them whenever needed.

--- projects/isteven-multiselect/src/lib/isteven-multiselect-base.component.ts
+++ projects/isteven-multiselect/src/lib/isteven-multiselect-base.component.ts
@@ -3,13 +3,37 @@ import { IstevenMultiselectService } from './services/isteven-multiselect.servic
 import { ControlValueAccessor } from '@angular/forms';
 
 export abstract class IstevenMultiselectBaseComponent implements ControlValueAccessor {
+  
   private _istevenMultiselectService: IstevenMultiselectService;
   private _elementRef: ElementRef;
+  private operationPendingQueue: any[] = [];
+  abstract _options: any[];
 
   constructor(
     protected injector: Injector) {
       this._elementRef = injector.get(ElementRef);
       this._istevenMultiselectService = injector.get(IstevenMultiselectService);
+  }
+
+  // Adding pending operation in queue
+  addOperation(item) {
+    this.operationPendingQueue.push(item)
+  }
+
+  // Poping pending operation from queue sequentially
+  popOperation() {
+    return this.operationPendingQueue.pop()
+  }
+
+  // Extracting and finishing all pending operation
+  finishOperation() {
+    let operation = this.popOperation();
+    this.prepopulateOptions(operation);
+  }
+
+  // Check pending operation queue status
+  isOperationPending() {
+    return this.operationPendingQueue.length
   }
 
   private _initialValue: any;
@@ -26,6 +50,7 @@ export abstract class IstevenMultiselectBaseComponent implements ControlValueAcc
 
   writeValue(value) {
     if(value) this.initialValue = value
+    if(!this._options) this.addOperation(value)
     // Set selected value for initial load of value
     this.prepopulateOptions(value);
   }
