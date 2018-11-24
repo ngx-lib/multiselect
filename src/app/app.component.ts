7d34c8f4e1ffd4bec4ce6927f62e99c14ef81727
Pankaj Parkar <parkarpankaj3@gmail.com>
2018-11-24 23:16:40 +0530

    Added kindof support for Observable (only with async options). Segregated options list into service.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'im-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  singleSelectOptions;
+  multipleSelectOptions;
+  groupingOptions;
+  observableOptions;
+
+  constructor (private appService: AppService) {}
+
   singleSelect: any = {id: 1, name: 'a'}
   mutlipleSelect = new FormControl([
     {id: 1, name: 'Test 1'},
@@ -18,43 +27,13 @@ export class AppComponent {
   withObservable = new FormControl([
     {id: 1, name: 'Test 1'},
   ]);
-  // options = [
-  //   'a', 'b', 'c', 'd', 'e', 'f'
-  // ]
-  
-  // propertyMap = {
-  //   name: 'value'
-  // }
-  
-  singleSelectOptions = [
-    {id: 1, name: 'a'},
-    {id: 2, name: 'b'},
-    {id: 3, name: 'c'},
-    {id: 4, name: 'd', disabled: true},
-    {id: 5, name: 'e'},
-  ]
 
-  multipleSelectOptions = [
-    {id: 1, name: 'Test 1'},
-    {id: 2, name: 'Test 2'},
-    {id: 3, name: 'Test 3'},
-    {id: 4, name: 'Test 4'},
-    {id: 5, name: 'Test 5'}
-  ]
-
-  observableOptions = [
-    {id: 1, name: 'Test 1'},
-    {id: 2, name: 'Test 2'},
-    {id: 3, name: 'Test 3'},
-    {id: 4, name: 'Test 4'},
-    {id: 5, name: 'Test 5'}
-  ]
-
-  groupingOptions = [
-    {id: 1, name: 'Test 1', category: 'Cat 1'},
-    {id: 2, name: 'Test 2', category: 'Cat 1'},
-    {id: 3, name: 'Test 3', category: 'Cat 2'},
-    {id: 4, name: 'Test 4', category: 'Cat 2'},
-    {id: 5, name: 'Test 5', category: 'Cat 3'}
-  ]
+  ngOnInit(): void {
+    this.singleSelectOptions = this.appService.getSingleSelectOptions()
+    this.multipleSelectOptions = this.appService.getMultipleSelectOptions()
+    this.groupingOptions = this.appService.getGroupingOptions()
+    setTimeout(() => {
+      this.observableOptions = this.appService.getObservableOptions()
+    }, 5000)
+  }
 }
