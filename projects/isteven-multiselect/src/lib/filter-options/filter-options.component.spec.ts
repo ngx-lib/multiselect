ff263d531fbef04f3ffc60adc6718fa30d4b00b0
Pankaj Parkar <parkarpankaj3@gmail.com>
2018-12-06 00:25:02 +0530

    Jotted down the possible testcases 1. filter-options tests 2. grouped-options tests 3. options tests 4. filter-options tests

--- projects/isteven-multiselect/src/lib/filter-options/filter-options.component.spec.ts
+++ projects/isteven-multiselect/src/lib/filter-options/filter-options.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FilterOptionsComponent } from './filter-options.component';
 
-describe('FilterOptionsComponent', () => {
+describe('GroupedOptionsComponent', () => {
   let component: FilterOptionsComponent;
   let fixture: ComponentFixture<FilterOptionsComponent>;
 
@@ -19,7 +19,36 @@ describe('FilterOptionsComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('Component should gets added into the DOM', () => {
+    expect(true).toBeTruthy();
   });
+
+  it('By default input elemenet is focused', () => {
+    expect(true).toBeTruthy();
+  });
+
+  describe('Input text change', () => {
+    it('Adding text into input box, should filter the result', () => {
+      expect(true).toBeTruthy();
+    });
+    it('Removing text into input box, should filter the result', () => {
+      expect(true).toBeTruthy();
+    });
+    it('for empty input field, it should show all optoin', () => {
+      expect(true).toBeTruthy();
+    });
+    it('initially all group options selected, removal any of them should unmark group option', () => {
+      expect(true).toBeTruthy();
+    });
+  })
+
+  describe('Clear button', () => {
+    it('it should emit an event to parent component', () => {
+      expect(true).toBeTruthy();
+    });
+    it('it should clear all selected options', () => {
+      expect(true).toBeTruthy();
+    });
+  })
+  
 });
--- projects/isteven-multiselect/src/lib/helper-elements/helper-elements.component.spec.ts
+++ projects/isteven-multiselect/src/lib/helper-elements/helper-elements.component.spec.ts
@@ -19,7 +19,44 @@ describe('HelperElementsComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('Component should gets added into the DOM', () => {
+    expect(true).toBeTruthy();
   });
+
+  describe('✓ Select All', () => {
+    it('it should emit an event to parent component', () => {
+      expect(true).toBeTruthy();
+    });
+    it('it should select all options', () => {
+      expect(true).toBeTruthy();
+    });
+  })
+
+  describe('× Select None', () => {
+    it('it should emit an event to parent component', () => {
+      expect(true).toBeTruthy();
+    });
+    it('button should de-select all options', () => {
+      expect(true).toBeTruthy();
+    });
+  })
+  
+  describe('↶ Reset', () => {
+    it('it should emit an event to parent component', () => {
+      expect(true).toBeTruthy();
+    });
+    it('It should reset dropdown value to older state', () => {
+      expect(true).toBeTruthy();
+    });
+  })
+
+  describe('Select all and select none button should be', () => {
+    it('visible only in case of multiple select', () => {
+      expect(true).toBeTruthy();
+    })
+    it('invisible incase of single select', () => {
+      expect(true).toBeTruthy();
+    })
+  })
+
 });
