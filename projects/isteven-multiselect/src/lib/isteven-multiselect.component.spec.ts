a45eeb10b866b73c897f0d51db168d0b8ef1897b
Pankaj Parkar <parkarpankaj3@gmail.com>
2018-12-07 23:51:41 +0530

    Adding testcases text, and later they will be filled in.

--- projects/isteven-multiselect/src/lib/isteven-multiselect.component.spec.ts
+++ projects/isteven-multiselect/src/lib/isteven-multiselect.component.spec.ts
@@ -19,7 +19,65 @@ describe('IstevenMultiselectComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(true).toBeTruthy();
+  // isOpen flag
+  describe('IsOpen flag', () => {
+    it('On click of button isOpen property should be true', () => {
+      expect(true).toBeTruthy();
+    });
+    it('On click outside drodown, it should close dropdown', () => {
+      expect(true).toBeTruthy();
+    });
+    it('clicking on open dropdown button should change make isOpen false', () => {
+      expect(true).toBeTruthy();
+    });
+    it('clicking on open dropdown button should change make isOpen false', () => {
+      expect(true).toBeTruthy();
+    });
+    it('when isOpen true, helper element, filter options and options should exists', () => {
+      expect(true).toBeTruthy();
+    });
+  });
+
+  // Template
+  describe('Templating', () => {
+    it('should pass on older template incase none of the opton is passed', () => {
+      expect(true).toBeTruthy();
+    });
+    it('should pass on newer template incase none of the opton is passed', () => {
+      expect(true).toBeTruthy();
+    });
+    it('should pass on newer template for grouping, incase none of the opton is passed', () => {
+      expect(true).toBeTruthy();
+    });
+    it('should pass on newer template for grouping, incase none of the opton is passed', () => {
+      expect(true).toBeTruthy();
+    });
+  });
+
+  // on clear
+  describe('on clear button click', () => {
+    it('should remove value from single select', () => {
+      expect(true).toBeTruthy();
+    });
+    it('should remove value from multiple select', () => {
+      expect(true).toBeTruthy();
+    });
+  });
+
+  describe('Single Select', () => {
+    it('When it has no value, should show "None Selected"', () => {
+      expect(true).toBeTruthy();
+    });
+    it('should show "name" property of an selected value', () => {
+      expect(true).toBeTruthy();
+    });
+    it('should show "name" property of an selected value', () => {
+      expect(true).toBeTruthy();
+    });
+  });
+  describe('Multiple Select', () => {
+    it('Multiple Select', () => {
+      expect(true).toBeTruthy();
+    });
   });
 });
