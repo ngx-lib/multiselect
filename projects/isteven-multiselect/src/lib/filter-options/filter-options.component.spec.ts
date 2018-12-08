bcfbecc648c367eaed898a09d33f67b8280cd05d
Pankaj Parkar <parkarpankaj3@gmail.com>
2018-12-08 14:15:03 +0530

    Fixed all blank test cases to be passed.

--- projects/isteven-multiselect/src/lib/filter-options/filter-options.component.spec.ts
+++ projects/isteven-multiselect/src/lib/filter-options/filter-options.component.spec.ts
@@ -1,14 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { BrowserModule } from '@angular/platform-browser';
 
 import { FilterOptionsComponent } from './filter-options.component';
 
-describe('GroupedOptionsComponent', () => {
+describe('FilterOptionsComponent', () => {
   let component: FilterOptionsComponent;
   let fixture: ComponentFixture<FilterOptionsComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FilterOptionsComponent ]
+      declarations: [ FilterOptionsComponent ],
+      imports: [FormsModule, ReactiveFormsModule, BrowserModule]
     })
     .compileComponents();
   }));
