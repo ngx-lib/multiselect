b4a4e907f16d6e5fc6fdd7265dcc56e1a8f729fa
pankajparkar <parkarpankaj3@gmail.com>
2018-12-15 16:37:29 +0530

    Moved DEFAULT_VALUE_ACCESSOR back to multiselect.component.ts

--- projects/multiselect/src/public_api.ts
+++ projects/multiselect/src/public_api.ts
@@ -5,7 +5,6 @@
 export * from './lib/multiselect.module';
 export * from './lib/pipes/display-selected-value.pipe';
 export * from './lib/services/multiselect.service';
-export * from './lib/services/default-value-accessor';
 export * from './lib/grouped-options/grouped-options.component';
 export * from './lib/options/options.component';
 export * from './lib/filter-options/filter-options.component';
