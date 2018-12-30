ecf566c619359b24476df534cb4a15ea455bc43a
Pankaj Parkar <parkarpankaj3@gmail.com>
2018-12-30 10:41:30 +0530

    Added virtual scroll directive in a place.

---
+++ projects/multiselect/src/lib/directives/virtual-scroll.directive.spec.ts
@@ -0,0 +1,8 @@
+import { VirtualScrollDirective } from './virtual-scroll.directive';
+
+describe('VirtualScrollDirective', () => {
+  it('should create an instance', () => {
+    const directive = new VirtualScrollDirective();
+    expect(directive).toBeTruthy();
+  });
+});
