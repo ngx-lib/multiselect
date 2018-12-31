9664be7dc1794a996c9024b23e2473d0d76d9a1a
Pankaj Parkar <parkarpankaj3@gmail.com>
2018-12-31 20:25:01 +0530

    Written comments for steps to be followed while implemented solution.

--- projects/multiselect/src/lib/directives/virtual-scroll.directive.ts
+++ projects/multiselect/src/lib/directives/virtual-scroll.directive.ts
@@ -1,17 +1,23 @@
-import { Directive, HostListener } from '@angular/core';
+import { Directive, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[msVirtualScroll]'
 })
 export class VirtualScrollDirective {
   scrollOffset = 16
-  defualtHeight = 40
+  @Input() itemHeight: number = 40
+  @Input() totalCount: number
 
   constructor() { }
 
   @HostListener('scroll', ['$event']) onscroll({target}) {
     const {scrollToTop, scrollToBottom, scrollHeight, clientHeight} = target;
     console.log(event)
+    // Step: 1 - Calculate the position
+
+    // Step: 2 - What are the possible collection that can be rendered
+
+    // Step: 3 - Pass the range to the child directive (probably custom *ngFor)
   }
 
 }
