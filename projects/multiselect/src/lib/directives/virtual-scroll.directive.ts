07ec778cd169f16caa3d3ff1a2588ae49864b3cb
Pankaj Parkar <parkarpankaj3@gmail.com>
2018-12-31 20:35:50 +0530

    Added more calcuation variables those will be used in calculation.

--- projects/multiselect/src/lib/directives/virtual-scroll.directive.ts
+++ projects/multiselect/src/lib/directives/virtual-scroll.directive.ts
@@ -14,12 +14,14 @@ export class VirtualScrollDirective {
     const {scrollToTop, scrollToBottom, scrollHeight, clientHeight} = target;
     console.log(event)
     // Step: 1 - Calculate the position
-    const topPadding = scrollToTop;
-    const remainingHeight = clientHeight - topPadding;
+    const topSpacing = scrollToTop;
+    const remainingHeight = clientHeight - topSpacing;
 
     // Step: 2 - What are the possible collection that can be rendered
-    const rangeStart = topPadding
-    const rangeEnd = topPadding + clientHeight
+    const rangeStart = topSpacing
+    const rangeEnd = topSpacing + clientHeight
+    const difference = topSpacing % this.itemHeight
+    const topNonVisible = topSpacing / this.itemHeight
 
     // Step: 3 - Pass the range to the child directive (probably custom *ngFor)
   }
