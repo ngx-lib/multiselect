78d0a3cda7d1f2ad55440fef00a1aea5f17069c7
nitinkalenk <nitinkale.nk@gmail.com>
2019-01-23 20:38:24 +0530

    title, note, content comp using input for data and sample rendered

--- src/app/doc/doc.component.ts
+++ src/app/doc/doc.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DocComponent implements OnInit {
 
+  data: any = {
+    title : 'Sample Title',
+    content : 'Sample content to describe title',
+    note: 'Sample note in case needed'
+  };
+
   constructor() { }
 
   ngOnInit() {
--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'ms-title',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./title.component.css']
 })
 export class TitleComponent implements OnInit {
+
+  @Input()
+  title : string;
 
   constructor() { }
 
