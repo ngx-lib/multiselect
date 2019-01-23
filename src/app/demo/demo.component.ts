383a832dfb3d65414086f81f55fcffda73dde197
nitinkalenk <nitinkale.nk@gmail.com>
2019-01-23 21:47:05 +0530

    demo component rendered

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'ms-demo',
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DemoComponent implements OnInit {
 
+  @Input()
+  demoName: string;
+  
   constructor() { }
 
   ngOnInit() {
