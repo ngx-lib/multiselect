cc0b7f199b1e1334afe5abaf785040f9edd060b6
Nitin Kale <nitinkale.nk@gmail.com>
2019-01-25 18:03:23 +0530

    code-view component ready with highlight js

--- src/app/code-view/code-view.component.ts
+++ src/app/code-view/code-view.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
+import hljs from 'highlight.js/lib/highlight';
 
 @Component({
   selector: 'ms-code-view',
@@ -9,10 +10,16 @@ export class CodeViewComponent implements OnInit {
 
   @Input()
   code: string;
+  @Input()
+  language: string;
+  @ViewChild('code')
+  codeElement: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
+    this.codeElement.nativeElement.textContent = this.code;
+    hljs.highlightBlock(this.codeElement.nativeElement);
   }
 
 }
--- src/app/demo-container/demo-container.component.ts
+++ src/app/demo-container/demo-container.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import hljs from 'highlight.js/lib/highlight';
+import typescript from 'highlight.js/lib/languages/typescript';
+import xml from 'highlight.js/lib/languages/xml';
+import css from 'highlight.js/lib/languages/css';
+hljs.registerLanguage('xml', xml);
+hljs.registerLanguage('css', css);
+hljs.registerLanguage('typescript', typescript);
 
 @Component({
   selector: 'ms-demo-container',
@@ -7,9 +14,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DemoContainerComponent implements OnInit {
 
+  tsCode: string;
+  htmlCode: string;
+  cssCode: string;
+
   constructor() { }
 
   ngOnInit() {
+    this.tsCode = `import { Component, OnInit } from '@angular/core';
+
+    @Component({
+      selector: 'ms-demo-container',
+      templateUrl: './demo-container.component.html',
+      styleUrls: ['./demo-container.component.css']
+    })
+    export class DemoContainerComponent implements OnInit {
+    
+      tsCode: string;
+      htmlCode: string;
+      cssCode: string;
+    
+      constructor() { }
+    
+      ngOnInit() {
+        this.cssCode = '';
+      }
+    
+    }`;
+    this.cssCode = `
+    .text-indent{
+      text-indent: 5rem;
+    }`;
+    this.htmlCode = `
+    <h2>Custom Template</h2>
+    `;
+
   }
 
 }
