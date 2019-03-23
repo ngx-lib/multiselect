405d388d2a4dbbea9fa58d46724bf0fc6420b86b
Pankaj Parkar <parkarpankaj3@gmail.com>
2019-03-23 22:29:32 +0530

    Updated navigation and contribution list.

--- docs_app/src/app/custom-elements/demo/observable-dynamic-update/observable-dynamic-update.module.ts
+++ docs_app/src/app/custom-elements/demo/observable-dynamic-update/observable-dynamic-update.module.ts
@@ -15,6 +15,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   declarations: [ ObservableDynamicUpdateComponent ],
   entryComponents: [ ObservableDynamicUpdateComponent ]
 })
-export class CurrentLocationModule implements WithCustomElementComponent {
+export class ObservableDynamicUpdateModule implements WithCustomElementComponent {
   customElementComponent: Type<any> = ObservableDynamicUpdateComponent;
 }
