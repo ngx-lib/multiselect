c49ec37913c9bad64d380084cea13264ef539e56
Pankaj Parkar <parkarpankaj3@gmail.com>
2019-03-26 22:56:18 +0530

    Added examples folder for all docs md file.

--- docs_app/content/examples/getting-started/src/app/app.module.ts
+++ docs_app/content/examples/getting-started/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   imports: [ BrowserModule ],
   declarations: [
     AppComponent,
+    HighlightDirective
   ],
   bootstrap: [ AppComponent ]
 })
