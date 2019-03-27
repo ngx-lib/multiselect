c7e26b3fe3dc67fbb8cded3e6eff16b6a241a227
Pankaj Parkar <parkarpankaj3@gmail.com>
2019-03-27 22:11:44 +0530

    Removed unwanted files from example folder.

--- docs_app/content/examples/events/src/app/app.module.ts
+++ docs_app/content/examples/events/src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   imports: [ BrowserModule ],
   declarations: [
-    AppComponent,
-    HighlightDirective
+    AppComponent
   ],
   bootstrap: [ AppComponent ]
 })
