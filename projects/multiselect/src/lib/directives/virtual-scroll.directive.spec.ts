214a37ae9d4d29b38d4a14423f4dc1818e5a6958
Pankaj Parkar <parkarpankaj3@gmail.com>
2019-07-05 20:40:46 +0530

    Added placeholder for test cases.

--- projects/multiselect/src/lib/directives/virtual-scroll.directive.spec.ts
+++ projects/multiselect/src/lib/directives/virtual-scroll.directive.spec.ts
@@ -6,4 +6,20 @@ describe('VirtualScrollDirective', () => {
     const directive = new VirtualScrollDirective(<ElementRef>{}, <Renderer2>{});
     expect(directive).toBeTruthy();
   });
+  it('should call initialSetup method when count is more than zero', () => {
+    const directive = new VirtualScrollDirective(<ElementRef>{}, <Renderer2>{});
+    expect(directive).toBeTruthy();
+  });
+  it('should call reset method when count is zero', () => {
+    const directive = new VirtualScrollDirective(<ElementRef>{}, <Renderer2>{});
+    expect(directive).toBeTruthy();
+  });
+  it('should scroll and show next items of the list', () => {
+    const directive = new VirtualScrollDirective(<ElementRef>{}, <Renderer2>{});
+    expect(directive).toBeTruthy();
+  });
+  it('should show only elements which are present in scrolling div', () => {
+    const directive = new VirtualScrollDirective(<ElementRef>{}, <Renderer2>{});
+    expect(directive).toBeTruthy();
+  });
 });
