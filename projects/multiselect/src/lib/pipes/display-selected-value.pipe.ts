9240d7814f14a5cbe38861c96fdfc66d68261db5
Calut <jedie.boy.d.calut@accenture.com>
2020-10-20 11:55:46 +0800

    add total items when select maxlabel is true

--- projects/multiselect/src/lib/pipes/display-selected-value.pipe.ts
+++ projects/multiselect/src/lib/pipes/display-selected-value.pipe.ts
@@ -10,11 +10,13 @@ export class DisplaySelectedValuePipe implements PipeTransform {
   defaultMaxLabelCount: number = 3;
   transform(value: any, maxLabelCount: number = this.defaultMaxLabelCount): any {
     if (value instanceof Array) {
+
       const collection = [...value];
+      var total = collection.length;
       const condn = collection.length > maxLabelCount;
       condn ? (collection.length = maxLabelCount) : null;
 
-      return `${collection.map(v => v.name).join(', ')}${condn ? DOTS : ''}`;
+      return `${collection.map(v => v.name).join(', ')}${condn ? DOTS + ' ('+total+')' : ''}`;
     }
     return value && value.name;
   }
